c227f1a5a844292e06e99d9fd384c434bb3fa86a
Akshayudayakumar <113658103+Akshayudayakumar@users.noreply.github.com>
2025-02-28 14:46:22 +0530

    Update idProof.dart

---
+++ lib/view/profile_screen/widgets/idProof.dart
@@ -0,0 +1,58 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class IdProofUpload extends StatefulWidget {
+  @override
+  _IdProofUploadState createState() => _IdProofUploadState();
+}
+
+class _IdProofUploadState extends State<IdProofUpload> {
+  TextEditingController controller = TextEditingController();
+  File? selectedFile;
+
+  Future<void> pickDocument() async {
+    final pickedFile = await ImagePicker().pickImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      setState(() {
+        selectedFile = File(pickedFile.path);
+        controller.text = pickedFile.path.split('/').last; // Show only file name
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(width: MediaQuery.of(context).size.width,
+      height: 150,decoration: BoxDecoration(
+            color: Colors.black.withValues(alpha: 0.3),
+            borderRadius: BorderRadius.circular(10)),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TextFormField(
+            controller: controller,
+            readOnly: true,
+            decoration: InputDecoration(
+              labelText: "Upload ID Proof (Aadhaar, License)",
+              suffixIcon: IconButton(
+                icon: Icon(Icons.upload_file),
+                onPressed: pickDocument,
+              ),
+            ),
+          ),
+          SizedBox(height: 10),
+          if (selectedFile != null) // Show preview if file is selected
+            Column(
+              children: [
+                Text("Preview:"),
+                SizedBox(height: 5),
+                Image.file(selectedFile!, width: 150, height: 100, fit: BoxFit.cover),
+              ],
+            ),
+        ],
+      ),
+    );
+  }
+}
